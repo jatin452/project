6f5b93d09ba96ee7ac800b7f440f6a76c8b4da7c
Jatin Dalla <78598294+jatin452@users.noreply.github.com>
2024-03-19 14:15:42 +0530

    Add files via upload

---
+++ Sample.ps1
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# This is a sample shell script
+echo "Hello from the shell script!"
+echo "This script is running as part of the build process."
+echo "You can add any commands or logic here that you need for your build."

0c53fcf97900c50393ae6c336ab3ee28d4690dd3
Jatin Dalla <78598294+jatin452@users.noreply.github.com>
2024-04-08 10:58:07 +0530

    Update Sample.ps1

--- Sample.ps1
+++ Sample.ps1
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 # This is a sample shell script
-echo "Hello from the shell script!"
-echo "This script is running as part of the build process."
-echo "You can add any commands or logic here that you need for your build."
+Write-Host "Hy"
